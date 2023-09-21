36298ae1602324051a24c9fbaf8cac25b138f634
Aleksander Długosz <olekdlugi@gmail.com>
2023-09-21 22:07:24 +0200

    Added art, style and structure

--- src/index.tsx
+++ src/index.tsx
@@ -6,25 +6,81 @@ import { Post, PostsDatabase } from "./posts";
 const app = new Elysia()
     .use(html())
     .decorate('db', new PostsDatabase())
-    .get("/", ({html}) => html(
+    .get("/", ({set}) => set.redirect = "/posts")
+    .get("/posts", ({html}) => html(
         <BaseHtml>
-            <h2>Moomba's Seaside Port</h2>
-            <h3>New post form</h3>
-            <div class="section"><PostForm/></div>
-            <h3>Posts</h3>
-            <div class="section" hx-get="/posts" hx-trigger="load" hx-swap="innerHTML" id="posts"></div>
+            <div class="content">
+                <div id="header">
+                    <div class="title">Moomba's Seaside Port</div>
+                    <div class="navigation">
+                        <ul>
+                            <li>Posts</li>
+                            <li>About Me</li>
+                            <li>Gallery</li>
+                        </ul>
+                        <ul>
+                            <li>#all</li>
+                            <li>#shadowhaven</li>
+                            <li>#tgom</li>
+                        </ul>
+                    </div>
+                    <img class="art" src="/art/art1@2x.png" alt="A road leading to a castle in the distance."/>
+                </div>
+                <div id="posts" hx-get="/api/posts" hx-trigger="load" hx-swap="innerHTML"></div>
+                <div id="table-of-contents">
+                    <h1>Table of contents</h1>
+                    <div class="table-of-contents__tags">
+                        <a href="#">#all</a>
+                        <a href="#">#java</a>
+                        <a href="#">#c++</a>
+                        <a href="#">#godot</a>
+                        <a href="#">#unity3d</a>
+                        <a href="#">#python</a>
+                        <a href="#">#photosh</a>
+                        <a href="#">#substance-painter</a>
+                        <a href="#">#blender</a>
+                        <a href="#">#animation</a>
+                        <a href="#">#web-design</a>
+                        <a href="#">#beginner</a>
+                        <a href="#">#shadowhaven</a>
+                        <a href="#">#tgom</a>
+                    </div>
+                </div>
+                <div id="footer">
+                    <div class="footer__cut-line"></div>
+                    <div class="footer__content">
+                        <div class="footer__content__copyright">
+                            <p>© 2023 Aleksander Długosz</p>
+                            <p>"Raiders roll"</p>
+                        </div>
+                        <div class="footer__content__socials">
+                            <a class="link-email" href="mailto:olekdlugi@gmail.com" target="_blank">olekdlugi@gmail.com</a>
+                            <a class="link-phone" href="tel:+48505873740" target="_blank">+48 505 873 740</a>
+                            <a class="link-linkedin" href="https://www.linkedin.com/in/adlugosz/" target="_blank">in/adlugosz</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </BaseHtml>
     ))
-    .get("/posts", async ({db}) => <PostList posts={await db.list()}/>)
-    .post("/posts", async ({db, body}) => {
+    .get("/admin", ({html}) => html(
+        <BaseHtml>
+            <h3>New post form</h3>
+            <div class="section"><PostForm/></div>
+        </BaseHtml>
+    ))
+    .get("/api/posts", async ({db}) => <PostList posts={await db.list()}/>)
+    .post("/api/posts", async ({db, body}) => {
+        const {title, content, createdAt} = body as any;
         await db.add({
-            title: body.title,
-            content: body.content,
-            createdAt: new Date(body.createdAt)
+            title: title,
+            content: content,
+            createdAt: new Date(createdAt)
         }).catch(console.error);
         return <PostList posts={await db.list()}/>;
     })
     .get("/styles.css", () => Bun.file("src/styles.css"))
+    .get("/art/:file", ({params: {file}}) => Bun.file(`art/${file}`))
     .listen(3000);
 
 const BaseHtml = ({children}: elements.Children) => "<!DOCTYPE html>" + (
@@ -42,9 +98,9 @@ const BaseHtml = ({children}: elements.Children) => "<!DOCTYPE html>" + (
 
 const PostItem = ({title, content, createdAt}: Post) => (
     <div class="post">
-        <h3>{title}</h3>
-        <p>{content}</p>
         <sub>{createdAt}</sub>
+        <h1>{title}</h1>
+        <p>{content}</p>
     </div>
 );
 
@@ -62,4 +118,3 @@ const PostForm = () => (
         <input type="submit" value="Submit"/>
     </form>
 )
-
