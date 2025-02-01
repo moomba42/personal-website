f882c926d061bef53616f2f29fd06388c8c7e4b9
Aleksander Długosz <olekdlugi@gmail.com>
2025-02-01 14:04:59 +0100

    Added dark reader lock to prevent the plugin from changing colors

--- src/index.tsx
+++ src/index.tsx
@@ -56,6 +56,7 @@ const BaseHtml = ({children}: elements.Children) => "<!DOCTYPE html>" + (
     <html lang="en">
     <head>
         <title>Moomba's Seaside Port</title>
+        <meta name="darkreader-lock"/>
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0"/>
         <link rel="stylesheet" href="styles.css"/>
         <script src="https://unpkg.com/htmx.org@1.9.5"></script>
